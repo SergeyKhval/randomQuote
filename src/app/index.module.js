b94a6339678946b103a6110d02834abd58617594
SergeyKhval <sergeykhval@gmail.com>
2016-03-21 16:44:43 +0300

    removed unnecessary dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,9 +4,10 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { WikiQuote } from '../app/components/wikiquote';
 
-angular.module('randomquote', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr'])
+angular.module('randomquote', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ngRoute', 'ui.bootstrap'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
   .service('WikiQuote', WikiQuote)
   .controller('MainController', MainController);
+//as
